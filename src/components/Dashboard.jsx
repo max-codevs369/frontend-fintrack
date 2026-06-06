3d501193d83527ae2556b3e53bdb7868248bfbdf
xvoid <ghanigustio28@gmail.com>
2026-06-06 16:51:55 +0700

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -32,6 +32,7 @@ export default function Dashboard() {
   const authHeaders = {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${token}`,
+    'ngrok-skip-browser-warning': '1731',
   };
 
   const handleLogout = () => {
