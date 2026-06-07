01137c6fa794ceb59047b980369b9884dc4a0690
max-codevs369 <ghanigustio28@gmail.com>
2026-06-08 00:02:58 +0700

    update feat pwa

--- vite.config.js
+++ vite.config.js
@@ -16,13 +16,25 @@ export default defineConfig({
         start_url: '/', 
         display: 'standalone',
         icons: [
-        { 
-          src: '/icon-512.png', 
-          sizes: '512x512', 
-          type: 'image/png',
-          purpose: 'any maskable' 
-        }
-      ]
+          { 
+            src: '/icon-512.png', 
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any' 
+          },
+          { 
+            src: '/icon-512.png', 
+            sizes: '512x512', 
+            type: 'image/png',
+            purpose: 'any' 
+          },
+          { 
+            src: '/icon-512.png', 
+            sizes: '512x512', 
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
       }
     })
   ]
